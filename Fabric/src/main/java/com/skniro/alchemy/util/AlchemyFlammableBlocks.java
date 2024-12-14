48f25e5354369e40e3c951f4513d3181762a95eb
skniro <46392259+skniro@users.noreply.github.com>
2024-12-14 18:10:57 +0800

    Flammable and strippable block.

--- Fabric/src/main/java/com/skniro/alchemy/util/AlchemyFlammableBlocks.java
+++ Fabric/src/main/java/com/skniro/alchemy/util/AlchemyFlammableBlocks.java
@@ -1,11 +1,19 @@
 package com.skniro.alchemy.util;
 
+import com.skniro.alchemy.block.AlchemyMapleBlocks;
 import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
 
 public class AlchemyFlammableBlocks {
     public static void registerFlammableBlocks() {
         FlammableBlockRegistry registry = FlammableBlockRegistry.getDefaultInstance();
 
+        registry.add(AlchemyMapleBlocks.MAPLE_LOG, 5, 5);
+        registry.add(AlchemyMapleBlocks.MAPLE_WOOD, 5, 5);
+        registry.add(AlchemyMapleBlocks.STRIPPED_MAPLE_LOG, 5, 5);
+        registry.add(AlchemyMapleBlocks.STRIPPED_MAPLE_WOOD, 5, 5);
+
+        registry.add(AlchemyMapleBlocks.MAPLE_PLANKS, 5, 20);
+        registry.add(AlchemyMapleBlocks.RED_MAPLE_LEAVES, 30, 60);
 
     }
 }
--- Fabric/src/main/java/com/skniro/alchemy/util/AlchemyStrippableBlocks.java
+++ Fabric/src/main/java/com/skniro/alchemy/util/AlchemyStrippableBlocks.java
@@ -1,7 +1,11 @@
 package com.skniro.alchemy.util;
 
+import com.skniro.alchemy.block.AlchemyMapleBlocks;
+import net.fabricmc.fabric.api.registry.StrippableBlockRegistry;
+
 public class AlchemyStrippableBlocks {
     public static void registerStrippables() {
-
+        StrippableBlockRegistry.register(AlchemyMapleBlocks.MAPLE_LOG, AlchemyMapleBlocks.STRIPPED_MAPLE_LOG);
+        StrippableBlockRegistry.register(AlchemyMapleBlocks.MAPLE_WOOD, AlchemyMapleBlocks.STRIPPED_MAPLE_WOOD);
     }
 }
